3e14df4f7415aa164585ea8ea0818ffa39e10095
Laukhil <laukhil23ti@mahasiswa.pcr.ac.id>
2025-11-20 10:07:34 +0700

    p11

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\Pelanggan;
@@ -10,82 +9,86 @@ class PelangganController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $data['dataPelanggan'] = Pelanggan::all();
+        $filterableColumns     = ['gender'];
+        $searchableColumns     = ['first_name']; //Tambahkan ini
+        $data['dataPelanggan'] = Pelanggan::filter($request, $filterableColumns)
+            ->search($request, $searchableColumns) // dan ini
+            ->paginate(10)->withQueryString();
         return view('admin.pelanggan.index', $data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('admin.pelanggan.create');
+        /**
+         * Show the form for creating a new resource.
+         */
+        public function create()
+        {
+            return view('admin.pelanggan.create');
+        }
+
+        /**
+         * Store a newly created resource in storage.
+         */
+        public function store(Request $request)
+        {
+            // dd($request->all());
+            $data['first_name'] = $request->first_name;
+            $data['last_name']  = $request->last_name;
+            $data['birthday']   = $request->birthday;
+            $data['gender']     = $request->gender;
+            $data['email']      = $request->email;
+            $data['phone']      = $request->phone;
+
+            Pelanggan::create($data);
+
+            return redirect()->route('pelanggan.index')->with('success', 'Penambahan Data Berhasil!');
+        }
+
+        /**
+         * Display the specified resource.
+         */
+        public function show(string $id)
+        {
+            //
+        }
+
+        /**
+         * Show the form for editing the specified resource.
+         */
+        public function edit(string $id)
+        {
+            $data['dataPelanggan'] = Pelanggan::findOrFail($id);
+            return view('admin.pelanggan.edit', $data);
+        }
+
+        /**
+         * Update the specified resource in storage.
+         */
+        public function update(Request $request, string $id)
+        {
+
+            $data['first_name'] = $request->first_name;
+            $data['last_name']  = $request->last_name;
+            $data['birthday']   = $request->birthday;
+            $data['gender']     = $request->gender;
+            $data['email']      = $request->email;
+            $data['phone']      = $request->phone;
+
+            Pelanggan::where('pelanggan_id', $id)->update($data);
+
+            return redirect()->route('pelanggan.index')
+                ->with('success', 'Perubahan Data Berhasil!');
+        }
+
+        /**
+         * Remove the specified resource from storage.
+         */
+        public function destroy(string $id)
+        {
+            //  dd('masuk destroy', $id);
+            $pelanggan = Pelanggan::findOrFail($id);
+            $pelanggan->delete();
+            return redirect()->route('pelanggan.index')->with('success', 'Data berhasil dihapus');
+        }
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        // dd($request->all());
-        $data['first_name'] = $request->first_name;
-        $data['last_name']  = $request->last_name;
-        $data['birthday']   = $request->birthday;
-        $data['gender']     = $request->gender;
-        $data['email']      = $request->email;
-        $data['phone']      = $request->phone;
-
-        Pelanggan::create($data);
-
-        return redirect()->route('pelanggan.index')->with('success', 'Penambahan Data Berhasil!');
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        $data['dataPelanggan'] = Pelanggan::findOrFail($id);
-        return view('admin.pelanggan.edit', $data);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        
-        $data['first_name'] = $request->first_name;
-        $data['last_name']  = $request->last_name;
-        $data['birthday']   = $request->birthday;
-        $data['gender']     = $request->gender;
-        $data['email']      = $request->email;
-        $data['phone']      = $request->phone;
-
-        Pelanggan::where('pelanggan_id', $id)->update($data);
-
-        return redirect()->route('pelanggan.index')
-            ->with('success', 'Perubahan Data Berhasil!');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-    //  dd('masuk destroy', $id);
-    $pelanggan = Pelanggan::findOrFail($id);
-    $pelanggan->delete();
-    return redirect()->route('pelanggan.index')->with('success', 'Data berhasil dihapus');
-    }
-}
